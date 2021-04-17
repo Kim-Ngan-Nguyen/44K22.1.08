e8fe2c8eb9c00bedfe20ce256fadaee6ddb2a58b
nhuvt9820 <74393345+nhuvt9820@users.noreply.github.com>
2021-04-17 09:13:51 +0700

    Add files via upload

--- Divider.dart
+++ Divider.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
-
 class DividerWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Divider(
-      height:1.0 ,
-      color: Colors.blue,
-      thickness: 3.0,
-
+      height: 1.0,
+      color: Colors.black,
+      thickness: 1.0,
     );
   }
 }
